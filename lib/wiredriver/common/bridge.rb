0870549f772d77b6e8b5471066fa7491f244b9c2
Sankha Narayan Guria <sankha93@gmail.com>
2016-06-04 11:28:15 +0530

    bridge can now make requests

--- lib/wiredriver/common/bridge.rb
+++ lib/wiredriver/common/bridge.rb
@@ -1,4 +1,5 @@
 require 'uri'
+require 'curb'
 
 module Wiredriver
   module Common
@@ -21,6 +22,28 @@ module Wiredriver
         @port = parsed_uri.port
         @userinfo = parsed_uri.userinfo
         @path = parsed_uri.path
+        @curl = Curl::Easy.new
+        if @userinfo
+          username, password = @userinfo.split ":"
+          @curl.http_auth_types = :basic
+          @curl.username = username
+          @curl.password = password
+        end
+        @curl.headers = { "Content-Type" => "application/json" }
+        @curl.follow_location = true
+      end
+
+      def request(method, path, payload = nil)
+        url_params = { :host => @host, :port => @port, :path => @path }
+        if @scheme == :http
+          current_url = URI::HTTP.build(url_params)
+        else
+          current_url = URI::HTTPS.build(url_params)
+        end
+        @curl.url = current_url
+        @curl.post_body = payload unless payload.nil?
+        @curl.http method.to_s.upcase
+        return @curl.status.to_i, @curl.body_str
       end
     end
   end
--- spec/bridge_spec.rb
+++ spec/bridge_spec.rb
@@ -1,26 +1,28 @@
+include Wiredriver::Common
+
 describe 'Wiredriver::Common::Bridge' do
   describe '#new' do
     before :each do
-      @url = "https://abc:xyz@example.com/pqrs/1234"
+      @url = "https://abc:xyz@example.com:2345/pqrs/1234"
     end
 
     it "creates a new remote HTTP bridge" do
-      @bridge = Wiredriver::Common::Bridge.new @url
-      expect(@bridge.class).to be(Wiredriver::Common::Bridge)
+      @bridge = Bridge.new @url
+      expect(@bridge.class).to be(Bridge)
     end
 
     it "throws if no URL is passed" do
-      expect { Wiredriver::Common::Bridge.new }.to raise_error(ArgumentError)
+      expect { Bridge.new }.to raise_error(ArgumentError)
     end
 
     it "throws if malformed URL is passed" do
       @url = "random"
-      expect { Wiredriver::Common::Bridge.new @url }.to raise_error(Wiredriver::Error::BridgeError)
+      expect { Bridge.new @url }.to raise_error(Wiredriver::Error::BridgeError)
     end
 
     context "parses the base URL params" do
       before :each do
-        @bridge = Wiredriver::Common::Bridge.new @url
+        @bridge = Bridge.new @url
       end
 
       it "should understand the scheme" do
@@ -32,7 +34,19 @@ describe 'Wiredriver::Common::Bridge' do
       end
 
       it "should get the port" do
+        expect(@bridge.port).to eq(2345)
+      end
+
+      it "should get the default port if unspecified" do
+        @url = "https://abc:xyz@example.com/pqrs/1234"
+        @bridge = Bridge.new @url
         expect(@bridge.port).to eq(443)
+      end
+
+      it "userinfo should be nil if none is specified" do
+        @url = "https://example.com:2345/pqrs/1234"
+        @bridge = Bridge.new @url
+        expect(@bridge.userinfo).to be(nil)
       end
 
       it "should get the basic auth details" do
@@ -42,6 +56,68 @@ describe 'Wiredriver::Common::Bridge' do
       it "should get the base path" do
         expect(@bridge.path).to eq("/pqrs/1234")
       end
+    end
+  end
+
+  describe "#request" do
+    before :each do
+      @bridge = Bridge.new "http://example.com"
+    end
+
+    context "sends a request" do
+      it "uses the method and path that is passed with Content-Type application/json" do
+        stub_request(:get, "http://example.com/").
+          with(:headers => {'Content-Type'=>'application/json'}).
+          to_return(:status => 200, :body => "boom", :headers => {})
+
+        status, body = @bridge.request :get, "/abcd"
+        expect(status).to eq(200)
+        expect(body).to eq("boom")
+      end
+
+      it "writes the request body when specified" do
+        stub_request(:post, "http://example.com/").
+         with(:body => "some data",
+              :headers => {'Content-Type'=>'application/json'}).
+         to_return(:status => 200, :body => "success", :headers => {})
+
+        status, body = @bridge.request :post, "/data", "some data"
+        expect(status).to eq(200)
+        expect(body).to eq("success")
+      end
+
+      it "uses basic auth when specified" do
+        url = "http://test:abcd@example.com"
+        @bridge = Bridge.new url
+        stub_request(:post, "http://example.com/").
+         with(:body => "some data",
+              :headers => {'Authorization'=>'Basic dGVzdDphYmNk', 'Content-Type'=>'application/json'}).
+         to_return(:status => 200, :body => "authed", :headers => {})
+
+        status, body = @bridge.request :post, "/data", "some data"
+        expect(status).to eq(200)
+        expect(body).to eq("authed")
+      end
+    end
+
+    it "throws an error in case the request failed" do
+      stub_request(:any, "example.com").to_timeout
+      expect { @bridge.request :get, "/" }.to raise_error(Curl::Err::TimeoutError)
+    end
+
+    it "follows 30x redirects" do
+      stub_request(:post, "http://example.com/").
+       with(:body => "some data",
+            :headers => {'Content-Type'=>'application/json'}).
+       to_return(:status => 301, :body => "", :headers => {'Location'=>'/final'})
+      stub_request(:post, "http://example.com/").
+       with(:body => "some data",
+            :headers => {'Content-Type'=>'application/json'}).
+       to_return(:status => 200, :body => "yay!", :headers => {})
+
+      status, body = @bridge.request :post, "/redirect", "some data"
+      expect(status).to eq(200)
+      expect(body).to eq("yay!")
     end
   end
 end
