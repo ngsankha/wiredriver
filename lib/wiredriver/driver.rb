11e85848a8efa004c93b3a8727336acce7da3757
Sankha Narayan Guria <sankha93@gmail.com>
2016-06-05 00:31:34 +0530

    refactored for cleaner code

--- lib/wiredriver/driver.rb
+++ lib/wiredriver/driver.rb
@@ -9,33 +9,31 @@ module Wiredriver
     attr_reader :session_id
 
     def initialize(url, capabilities)
-      wire_spec = COMMANDS["start_session"]
-
       @bridge = Bridge.new(url)
       @capabilities = capabilities
       request_body = JSON.generate({ "desiredCapabilities" => capabilities })
-      status, response_body = @bridge.request wire_spec[0], build_url(wire_spec[1], {}, @bridge.path), request_body
+      status, response_body = send_request("start_session", request_body, {})
       parsed_response = parse(response_body)
       @session_id = parsed_response["sessionId"]
     end
 
     def open_url(url)
-      wire_spec = COMMANDS["open_url"]
-
       request_body = JSON.generate({ "url" => url })
-      status, response_body = @bridge.request wire_spec[0],
-        build_url(wire_spec[1], { "sessionId" => @session_id }, @bridge.path), request_body
+      status, response_body = send_request("open_url", request_body)
       parse(response_body)
       self
     end
 
     def quit
-      wire_spec = COMMANDS["quit_session"]
-
-      status, response_body = @bridge.request wire_spec[0],
-        build_url(wire_spec[1], { "sessionId" => @session_id }, @bridge.path)
+      status, response_body = send_request("quit_session")
       parse(response_body)
       nil
+    end
+
+    private
+    def send_request(command, body = nil, params = { "sessionId" => @session_id })
+      method, path = COMMANDS[command]
+      @bridge.request method, build_url(path, params, @bridge.path), body
     end
   end
 end
